fe07e67e705706b68c219807bf7fc3777f8e36ec
Heronnes Pereira <heronnestg@hotmail.com>
2020-08-18 22:39:30 -0300

    parte 4

--- pasta27/redux-basics.js
+++ pasta27/redux-basics.js
@@ -1,8 +1,16 @@
-// Store
 
+const redux = require('redux')
+const criarStore = redux.createStore
 
 // Reducer
+const reducerRaiz = (state, action) => {
+    return state
+}
 
+
+// Store
+const armazenar = criarStore(reducerRaiz)
+console.log(armazenar.getState())
 
 // Dispatching Action
 
