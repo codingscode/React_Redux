24ccf61946f224dfc3bb46e5dc04823dd0971008
Heronnes Pereira <heronnestg@hotmail.com>
2020-08-18 23:13:13 -0300

    parte 7

--- pasta27/redux-basics.js
+++ pasta27/redux-basics.js
@@ -8,6 +8,16 @@ const stateInicial = {
 
 // Reducer
 const reducerRaiz = (state = stateInicial, action) => {
+    if (action.type === 'INC_CONTADOR') {
+        return {
+            ...state, contador: state.contador + 1
+        }
+    }
+    if (action.type === 'ADIC_CONTADOR') {
+        return {
+            ...state, contador: state.contador + action.valor
+        }
+    }
     return state
 }
 
@@ -19,7 +29,7 @@ console.log(armazenar.getState()) // undefined antes, { contador: 0 } depois
 // Dispatching Action
 armazenar.dispatch({type: 'INC_CONTADOR'})
 armazenar.dispatch({type: 'ADIC_CONTADOR', valor: 10})
-console.log(armazenar.getState())  // saida: { contador: 0 } 
+console.log(armazenar.getState())  // saida: { contador: 11 } 
 
 // Subscription
 
